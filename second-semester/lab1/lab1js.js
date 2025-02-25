560b2e15d236391e4295e9055a60745fe8476f9c
dip651 <maxgan651@gmail.com>
2025-02-25 21:52:22 +0200

    semester2 lab1 done

---
+++ second-semester/lab1/lab1js.js
@@ -0,0 +1,7 @@
+function change(msg){
+	let elem = document.getElementsByTagName("span")[0];
+	elem.textContent = msg;
+}
+
+let elem = document.getElementsByTagName("span")[0];
+elem.textContent = "Hello World!";
